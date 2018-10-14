8ae5de872450f9f20e495f300548a2c2ca892e14
Pier-Alexandre Trudeau <pier-alexandretrudeau@hotmail.ca>
2018-10-14 17:53:39 -0400

    Testing test

--- src/test/java/TestCalculerSomme.java
+++ src/test/java/TestCalculerSomme.java
@@ -1,12 +1,29 @@
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestCalculerSomme {
+	
+	private CSVReader reader = new CSVReader();
+	private List<Float> nombreListe;
+	
+	@Before
+	public void setUp() throws Exception {
+		nombreListe = reader.getNumberFromCSV("test.csv");
+	}
 
+	@After
+	public void tearDown() throws Exception {
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		System.out.println(nombreListe);
 	}
 
 }
